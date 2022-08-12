44131f966d7c53fa3613ce26cf04acd5880489e4
Richard Anaya <richard.anaya@gmail.com>
2022-08-12 15:28:52 -0700

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::event::{Event, KeyCode, KeyEvent};
+use crossterm::{event, terminal};
+use std::time::Duration;
+
+struct CleanUp;
+
+impl Drop for CleanUp {
+    fn drop(&mut self) {
+        terminal::disable_raw_mode().expect("Unable to disable raw mode")
+    }
+}
+
+fn main() -> crossterm::Result<()> {
+    let _clean_up = CleanUp;
+    terminal::enable_raw_mode()?;
+    loop {
+        if event::poll(Duration::from_millis(500))? {
+            if let Event::Key(event) = event::read()? {
+                match event {
+                    KeyEvent {
+                        code: KeyCode::Char('q'),
+                        modifiers: event::KeyModifiers::NONE,
+                        kind: _,
+                        state: _,
+                    } => break,
+                    _ => {
+                        //todo
+                    }
+                }
+                println!("{:?}\r", event);
+            };
+        } else {
+            println!("No input yet\r");
+        }
+    }
+    Ok(())
 }
